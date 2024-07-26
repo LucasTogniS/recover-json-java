211542e748fece929780d3513436f6c9d5cb20ca
LucasTogniS <65418585+LucasTogniS@users.noreply.github.com>
2024-07-26 10:51:00 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,41 @@
 package org.example;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Main {
-        public static void main(String[] args) {
-            String urlJson = "https://viacep.com.br/ws/01001000/json/";
-
-            try {
-                URL url = new URL(urlJson);
-                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
-
-                conexao.setRequestMethod("GET");
-                int responseCodeHttp = conexao.getResponseCode();
-
-                if (responseCodeHttp == HttpURLConnection.HTTP_OK) {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
-                    StringBuilder builder = new StringBuilder();
-
-                    String linha;
-
-                    while ((linha = bufferedReader.readLine()) != null) {
-                        builder.append(linha).append("\n");
-                    }
-
-                    bufferedReader.close();
-
-                    System.out.println(builder.toString());
-                } else {
-                    System.out.println("Erro: " + responseCodeHttp);
-                }
-            } catch (Exception e) {
-                System.err.println("Error " + e.getMessage());
+    private static final String urlJson = "https://viacep.com.br/ws/01001000/json/";
+    public static void main(String[] args) {
+        try {
+            String res = getResponse(urlJson);
+            System.out.println(res);
+        } catch (IOException e) {
+            System.err.println("Error " + e.getMessage());
         }
     }
-}
+        private static String getResponse (String urlExample) throws IOException {
+            URL url = new URL(urlExample);
+            HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+            conexao.setRequestMethod("GET");
+
+            int responseCodeHttp = conexao.getResponseCode();
+            if (responseCodeHttp == HttpURLConnection.HTTP_OK) {
+               try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conexao.getInputStream()))) {
+                   StringBuilder builder = new StringBuilder();
+                   String linha;
+
+                   while ((linha = bufferedReader.readLine()) != null) {
+                       builder.append(linha).append("\n");
+                   }
+                   return builder.toString();
+               }
+            } else {
+                throw new IOException("Code HTTP: " + responseCodeHttp);
+            }
+        }
+    }
+
+
